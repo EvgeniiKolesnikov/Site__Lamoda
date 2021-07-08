e72983869df13b3d1f07e51e3bda8a67ba749bbd
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-08 18:49:20 +0300

    Add day 3 scripts

--- scripts/script.js
+++ scripts/script.js
@@ -55,11 +55,11 @@ const getData = async () => {
   }
 }
 
-const getGoods = (callback, value) => {
+const getGoods = (callback, prop, value) => {
   getData()
     .then(data => {
       if (value) {
-        callback(data.filter(item => item.category === value));
+        callback(data.filter(item => item[prop] === value));
       } else {
         callback(data);
       }
@@ -90,7 +90,7 @@ subheaderCart.addEventListener('keydown', e => {
 
 const refreshLinks = () => {
   for (const link of navigationLinks) {
-    if (link.href.indexOf('#' + hash) !== -1) {
+    if (link.href.includes(location.hash)) {
       link.style = `border-bottom: 2px solid black;`
       goodsTitle.textContent = link.textContent;
     } else {
@@ -99,8 +99,9 @@ const refreshLinks = () => {
   }
 }
 
+// Страница категорий
 try {
-  console.log(hash);
+  // console.log(hash);
   const goodsList = document.querySelector('.goods__list');
   if (!goodsList) {
     throw 'This is not a goods page'
@@ -131,20 +132,20 @@ try {
 
   const renderGoodsList = data => {
     goodsList.textContent = ``;
-    console.log(data);
+    // console.log(data);
     data.forEach(item => {
       const card = createCard(item);
       goodsList.append(card);
     });
   }
 
-  getGoods(renderGoodsList, hash);
+  getGoods(renderGoodsList, 'category', hash);
   refreshLinks();
   
   window.addEventListener('hashchange', () => {
     hash = location.hash.substring(1);
     refreshLinks();
-    getGoods(renderGoodsList, hash);
+    getGoods(renderGoodsList, 'category', hash);
   })
 
 } catch (error) {
@@ -152,5 +153,69 @@ try {
 }
 
 
+// Страница товара
+try {
+  if (!document.querySelector('.card-good')) {
+    throw 'This is not a card-good page'
+  }
+
+  const cardGoodImage = document.querySelector('.card-good__image');
+  const cardGoodBrand = document.querySelector('.card-good__brand');
+  const cardGoodTitle = document.querySelector('.card-good__title');
+  const cardGoodPrice = document.querySelector('.card-good__price');
+  const cardGoodColor = document.querySelector('.card-good__color');
+  const cardGoodColorList = document.querySelector('.card-good__color-list');
+  const cardGoodSizes = document.querySelector('.card-good__sizes');
+  const cardGoodSizesList = document.querySelector('.card-good__sizes-list');
+  const cardGoodBuy = document.querySelector('.card-good__buy');
+  const cardGoodSelectWrapper = document.querySelectorAll('.card-good__select__wrapper');
+
+  const generateList = (data) => 
+    data.reduce((html, item, i) => html + 
+    `<li class="card-good__select-item" data-id="${i}">${item}</li>`, '');
+
+  const renderCardGood = ([{brand, name, cost, color, sizes, photo}]) => {
+    cardGoodImage.src = `goods-image/${photo}`;
+    cardGoodImage.alt = `${brand} ${name}`;
+    cardGoodBrand.textContent = brand;
+    cardGoodTitle.textContent = name;
+    cardGoodPrice.textContent = `${cost} ₽`;
+    if (color) {
+      cardGoodColor.textContent = color[0];
+      cardGoodColor.dataset.id = 0;
+      cardGoodColorList.innerHTML = generateList(color);
+    } else {
+      cardGoodColor.style.display = 'none';
+    }
+    if (sizes) {
+      cardGoodSizes.textContent = sizes[0];
+      cardGoodSizes.dataset.id = 0;
+      cardGoodSizesList.innerHTML = generateList(sizes);
+    } else {
+      cardGoodSizes.style.display = 'none';
+    }
+  };
+
+  cardGoodSelectWrapper.forEach(item => {
+    item.addEventListener('click', e => {
+      const target = e.target;
+      
+      if (target.closest('.card-good__select')) {
+        target.classList.toggle('card-good__select__open');
+      }
+
+      if (target.closest('.card-good__select-item')) {
+        const cardGoodSelect = item.querySelector('.card-good__select');
+        cardGoodSelect.textContent = target.textContent;
+        cardGoodSelect.dataset.id = target.dataset.id;
+        cardGoodSelect.classList.remove('card-good__select__open');
+      }
+    })
+  })
+  getGoods(renderCardGood, 'id', hash);
+
+} catch (error) {
+  console.warn('error: ', error);
+}
 
 
