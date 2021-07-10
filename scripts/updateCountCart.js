d76074e32a9d8f38644808b8e457fc29850c8d36
Evgenii Kolesnikov <jekow@yandex.ru>
2021-07-10 14:58:59 +0300

    Add declination of numerals

--- scripts/updateCountCart.js
+++ scripts/updateCountCart.js
@@ -9,10 +9,18 @@ export class UpdateCountCart {
     this.subheaderCart = document.querySelector(".subheader__cart");
   }
 
+  // возвращает только слово
+	declOfNum = (n, titles) => {
+		return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+			0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+	}
+
   updateCountCart() {
     if (this.storage.get(`${this.storageKey}`).length) {
       this.subheaderCart.textContent = 
-        this.storage.get(`${this.storageKey}`).length + ' товара';
+      this.declOfNum(
+        this.storage.get(`${this.storageKey}`).length,
+        ['товар', 'товара', 'товаров']);
     } else {
       this.subheaderCart.textContent = 'Корзина';
     }
